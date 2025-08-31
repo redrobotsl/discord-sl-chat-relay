41b7e93e41547b03093abe01bd5c8bddf289df11
Nolan Perry <nolantheperry@gmail.com>
2025-08-30 21:30:30 -0400

    DJS changed ready event to clientReady

--- DscEvents/clientReady.js
+++ DscEvents/clientReady.js
@@ -3,6 +3,6 @@ module.exports = async client => {
   // Log that the bot is online.
   logger.log(`${client.user.tag}, ready to serve ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} users in ${client.guilds.cache.size} servers.`, "ready");
   
-  // Make the bot "play the game" which is the help command with default prefix.
+/// this sets the activity, so the bot is being silly. 
   client.user.setActivity(`with the Metaverse`, { type: "PLAYING" });
 };
