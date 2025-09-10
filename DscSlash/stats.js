88a6a417d4d067527abaecd8b5178302ebc6eba3
Vault108 <44564111+Vault108@users.noreply.github.com>
2025-09-10 05:47:29 -0400

    Update stats.js
    remove inline from the embed to create cleaner formatting.

--- DscSlash/stats.js
+++ DscSlash/stats.js
@@ -15,14 +15,14 @@ exports.run = async (client, interaction) => { // eslint-disable-line no-unused-
     .setColor('#0099ff')
     .setTitle('STATISTICS')
     .addFields(
-      { name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+      { name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: false },
       { name: 'Uptime', value: duration, inline: true },
-      { name: 'Users', value: (client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)).toLocaleString(), inline: true },
-      { name: 'Servers', value: client.guilds.cache.size.toLocaleString(), inline: true },
-      { name: 'Channels', value: client.channels.cache.size.toLocaleString(), inline: true },
-      { name: 'Discord.js', value: `v${version}`, inline: true },
-      { name: 'Node.js', value: process.version, inline: true },
-      { name: 'Bot Version', value: `v${bot_version}`, inline: true },
+      { name: 'Users', value: (client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)).toLocaleString(), inline: false },
+      { name: 'Servers', value: client.guilds.cache.size.toLocaleString(), inline: false },
+      { name: 'Channels', value: client.channels.cache.size.toLocaleString(), inline: false },
+      { name: 'Discord.js', value: `v${version}`, inline: false },
+      { name: 'Node.js', value: process.version, inline: false },
+      { name: 'Bot Version', value: `v${bot_version}`, inline: false },
     )
     .setTimestamp();
 
