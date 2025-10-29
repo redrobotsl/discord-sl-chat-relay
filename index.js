b565042d641454a8db8368971d82c4f086dc3b13
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2025-10-29 00:58:44 -0400

    Fixes call to packageinfo location in index

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const RegionRestartHandler = require('./modules/RegionRestartHandler.js');
 // This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`,
 // or `bot.something`, this is what we're referring to. Your client.
-const packageinfo = require("../package.json");
+const packageinfo = require("./package.json");
 const client = new Client({ intents, partials });
 
 
